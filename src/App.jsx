5ec8d8ecdc41cb8202fc0753a53c0a3b6127a0dd
OnARCHIT <149319674+OnARCHIT@users.noreply.github.com>
2025-10-07 01:34:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import PhishingChallenges from "./components/PhishingChallenge.jsx";
 import PhishingRadar from "./components/PhishRadar.jsx";
 import ScanPanel from "./components/ScanPanel.jsx";
 
-import { startClipboardMonitor } from "./utils/clipboardMonitor.js";
+import { startClipboardMonitor } from "./helper/utils/clipboardMonitor.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
